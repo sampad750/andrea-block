3365f41045e294a25d0b78e5973038cd67d8dd2f
Sampad Debnath <sampad750@gmail.com>
2023-07-04 19:05:42 +0600

    add 4/07/2013

--- functions.php
+++ functions.php
@@ -14,9 +14,27 @@ if (!function_exists('andrea_setup')) :
 
 		// Enqueue editor styles.
 		add_editor_style('editor-style.css');
+		/*
+		* Load additional block styles.
+		*/
+		$styled_blocks = ['latest-comments'];
+		foreach ( $styled_blocks as $block_name ) {
+			$args = array(
+				'handle' => "myfirsttheme-$block_name",
+				'src'    => get_theme_file_uri( "assets/css/blocks/$block_name.css" ),
+				$args['path'] = get_theme_file_path( "assets/css/blocks/$block_name.css" ),
+			);
+			wp_enqueue_block_style( "core/$block_name", $args );
+		}
+
 	}
 endif; // andrea_setup
 add_action('after_setup_theme', 'andrea_setup');
 
 
 wp_enqueue_style( 'style', get_stylesheet_uri() );
+
+
+
+
+
