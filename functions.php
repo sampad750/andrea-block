1bbfa6d67b0e5281070ab4adf76ebbbe86ac3a98
Sampad Debnath <sampad750@gmail.com>
2023-07-29 11:36:37 +0600

    add 29/7/2023

--- functions.php
+++ functions.php
@@ -1,40 +1,76 @@
 <?php
+/**
+ * Andrea functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package andrea
+ * @since 1.0
+ */
 
-if (!function_exists('andrea_setup')) :
+if ( ! function_exists( 'andrea_theme_support' ) ) :
+
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
-	 * Note that this function is hooked into the after_setup_theme hook, which runs
-	 * before the init hook.
+	 * @since 1.0
+	 *
+	 * @return void
 	 */
-	function andrea_setup()
-	{
-		// Add support for block styles.
-		add_theme_support('wp-block-styles');
+	function andrea_theme_support() {
 
-		// Enqueue editor styles.
-		add_editor_style('editor-style.css');
-		/*
-		* Load additional block styles.
-		*/
-		$styled_blocks = ['latest-comments'];
-		foreach ( $styled_blocks as $block_name ) {
-			$args = array(
-				'handle' => "myfirsttheme-$block_name",
-				'src'    => get_theme_file_uri( "assets/css/blocks/$block_name.css" ),
-				$args['path'] = get_theme_file_path( "assets/css/blocks/$block_name.css" ),
-			);
-			wp_enqueue_block_style( "core/$block_name", $args );
-		}
+		// Add support for block styles.
+		add_theme_support( 'wp-block-styles' );
+
+		add_theme_support( 'woocommerce' );
 
 	}
-endif; // andrea_setup
-add_action('after_setup_theme', 'andrea_setup');
 
+endif;
 
-wp_enqueue_style( 'style', get_stylesheet_uri() );
+add_action( 'after_setup_theme', 'andrea_theme_support' );
 
+add_filter( 'woocommerce_prevent_automatic_wizard_redirect', '__return_true' );
 
+if ( ! function_exists( 'andrea_styles' ) ) :
 
+	/**
+	 * Enqueue styles.
+	 *
+	 * @since 1.0
+	 *
+	 * @return void
+	 */
+	function andrea_styles() {
+		// Register theme stylesheet.
+		$theme_version = wp_get_theme()->get( 'Version' );
 
+		$version_string = is_string( $theme_version ) ? $theme_version : false;
+		wp_enqueue_style(
+			'andrea-style',
+			get_template_directory_uri() . '/assets/css/style.css',
+			array(),
+			$version_string
+		);
+	}
 
+endif;
+
+add_action( 'wp_enqueue_scripts', 'andrea_styles' );
+
+function andrea_block_editor_styles() {
+	wp_enqueue_style( 'andrea-block-editor-style', get_theme_file_uri( '/css/block-editor.css' ), true, '1.0', 'all' );
+}
+add_action( 'enqueue_block_editor_assets', 'andrea_block_editor_styles' );
+
+// // Add block patterns
+// require get_template_directory() . '/inc/block-patterns.php';
+
+// // Add block styles
+// require get_template_directory() . '/inc/block-styles.php';
+
+// // Block Filters
+// require get_template_directory() . '/inc/block-filters.php';
+
+// // webfont Loader
+// require get_template_directory() . '/inc/wptt-webfont-loader.php';
